62c5c6049f53558e5e2f5f2fee5796d5c1755967
pedroezekiel <ezekieleniola36>
2025-03-25 11:26:00 +0100

    update routes

--- serializers/task_serializer.py
+++ serializers/task_serializer.py
@@ -6,15 +6,30 @@ from enums.task_status import TaskStatusEnum
 class TaskSerializer:
     @staticmethod
     def serialize(task):
-        """Convert Task object to a dictionary for MongoDB storage."""
-        # Extract correct status value
-        if isinstance(task.status, tuple):
-            task.status = task.status[0]  # Unpack tuple
-
         return {
-            "id": str(task.id),
+            "_id": task.get_id(),
             "title": task.title,
             "description": task.description,
             "status": task.status.value if isinstance(task.status, TaskStatusEnum) else str(task.status),
-            "created_at": task.created_at.isoformat() if isinstance(task.created_at, datetime) else None,
+            "created_at": task.created_at,
+            "date_completed": task.date_completed,
+            "date_updated": task.date_updated,
         }
+
+    @staticmethod
+    def deserialize(data: dict) -> dict:
+        return {
+            "_id": data["_id"],
+            "title": data["title"],
+            "description": data["description"],
+            "status": data["status"],  # Assuming it's already a string in MongoDB
+            "created_at": TaskSerializer.convert_datetime(data.get("created_at")),
+            "date_completed": TaskSerializer.convert_datetime(data.get("date_completed")),
+            "date_updated": TaskSerializer.convert_datetime(data.get("date_updated")),
+        }
+
+    @staticmethod
+    def convert_datetime(value):
+        if isinstance(value, str):
+            return datetime.fromisoformat(value)
+        return value
