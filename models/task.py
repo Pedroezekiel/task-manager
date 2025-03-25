da88382b73117678ea441952ed62475cfb98f62f
pedroezekiel <ezekieleniola36>
2025-03-25 10:01:55 +0100

    update flask model

--- models/task.py
+++ models/task.py
@@ -5,10 +5,13 @@ from enums.task_status import TaskStatusEnum
 class Task:
 
     def __init__(self, title, description, status=TaskStatusEnum.TO_DO):
-        self.id = str(uuid.uuid4())
+        self._id = str(uuid.uuid4())
         self.title = title
         self.description = description
-        self.status = status,
+        self.status = status
         self.created_at = datetime.datetime.now()
         self.date_completed = None
         self.date_updated = None
+
+    def get_id(self):
+        return self._id
