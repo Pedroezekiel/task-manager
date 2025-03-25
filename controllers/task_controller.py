9622a96632cf3bb01aa3eb1969c39e1ba9475679
pedroezekiel <ezekieleniola36>
2025-03-25 11:05:31 +0100

    update task controller

--- controllers/task_controller.py
+++ controllers/task_controller.py
@@ -12,5 +12,14 @@ class TaskController:
     def get_task(task_id):
         return TaskService.get_task(task_id)
 
+    @staticmethod
+    def update_task(task_id):
+        data = request.get_json()
+        return TaskService.update_task(task_id, data)
+
+    @staticmethod
+    def get_all_tasks():
+        return TaskService.get_all_tasks()
+
 
 
