69d365c6dca6362949c7b97069c80b8dd46e11ea
pedroezekiel <ezekieleniola36>
2025-03-26 10:01:23 +0100

    added auth

--- models/task.py
+++ models/task.py
@@ -4,13 +4,15 @@ from enums.task_status import TaskStatusEnum
 
 class Task:
 
-    def __init__(self, title, description, status=TaskStatusEnum.TO_DO):
+    def __init__(self, title, description,user_id, status=TaskStatusEnum.TO_DO):
         self._id = str(uuid.uuid4())
         self.title = title
         self.description = description
         self.status = status
         self.created_at = datetime.datetime.now()
         self.date_updated = None
+        self.updated_by = None
+        self.user_id = user_id
 
     def get_id(self):
         return self._id
