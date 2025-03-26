bf53c47d8f8e4e564aaf6b34ffb7a570d2a85b19
pedroezekiel <ezekieleniola36>
2025-03-26 13:53:53 +0100

    added org

--- app.py
+++ app.py
@@ -3,15 +3,19 @@ from flask_jwt_extended import JWTManager
 
 from config import Config
 from extensions.database import init_db
+from extensions.jwt import jwt,bcrypt
 from routes.task_routes import task_routes
 from routes.user_routes import user_routes
+from routes.organization_routes import organization_routes
 app = Flask(__name__)
 app.config.from_object(Config)
 init_db(app)
-jwt = JWTManager(app)
+jwt.init_app(app)
+bcrypt.init_app(app)
 
 app.register_blueprint(task_routes)
 app.register_blueprint(user_routes)
+app.register_blueprint(organization_routes)
 
 
 if __name__ == '__main__':
