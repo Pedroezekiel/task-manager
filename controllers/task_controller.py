edc0531fcb5810146e12a46e3a06c5e5ea0a4ee6
pedroezekiel <ezekieleniola36>
2025-03-26 10:02:10 +0100

    added auth

--- controllers/task_controller.py
+++ controllers/task_controller.py
@@ -1,33 +1,46 @@
 from flask import request
+from flask_jwt_extended import jwt_required, get_jwt, get_jwt_identity
 
 from service.task_service import TaskService
 
 class TaskController:
     @staticmethod
+    @jwt_required()
     def create_task():
+        user_id = get_jwt_identity()
         data = request.get_json()
-        return TaskService.create_task(data)
+        return TaskService.create_task(data, user_id)
 
     @staticmethod
+    @jwt_required()
     def get_task(task_id):
-        return TaskService.get_task(task_id)
+        user_id = get_jwt_identity()
+        return TaskService.get_task(task_id, user_id)
 
     @staticmethod
+    @jwt_required()
     def update_task(task_id):
+        user_id = get_jwt_identity()
         data = request.get_json()
-        return TaskService.update_task(task_id, data)
+        return TaskService.update_task(task_id, data, user_id)
 
     @staticmethod
+    @jwt_required()
     def get_all_tasks():
-        return TaskService.get_all_tasks()
+        user_id = get_jwt_identity()
+        return TaskService.get_all_tasks(user_id)
 
     @staticmethod
+    @jwt_required()
     def delete_task(task_id):
-        return TaskService.delete_task(task_id)
+        user_id = get_jwt_identity()
+        return TaskService.delete_task(task_id, user_id)
 
     @staticmethod
-    def complete_task(task_id, status):
-        return TaskService.update_task_status(task_id, status)
+    @jwt_required()
+    def change_task_status(task_id, status):
+        user_id = get_jwt_identity()
+        return TaskService.update_task_status(task_id, status, user_id)
 
 
 
