1db839802cd4a7db4be0e02ce30a45c7de617ab3
pedroezekiel <ezekieleniola36>
2025-03-27 12:46:41 +0100

    added organization task service

--- routes/organization_routes.py
+++ routes/organization_routes.py
@@ -15,5 +15,10 @@ class OrganizationRoutes(Blueprint):
         self.add_url_rule("/<org_id>", view_func=OrganizationController.edit_organization, methods=["PUT"])
         self.add_url_rule("/<org_id>", view_func=OrganizationController.view_organization, methods=["GET"])
         self.add_url_rule("/<site_name>/<org_id>", view_func=OrganizationController.delete_organization, methods=["DELETE"])
+        self.add_url_rule("/tasks", view_func=OrganizationController.organization_create_task, methods=["POST"])
+        self.add_url_rule("/tasks/<task_id>/<site_name>", view_func=OrganizationController.organization_edit_task, methods=["PUT"])
+        self.add_url_rule("/tasks/<task_id>/<site_name>", view_func=OrganizationController.organization_view_task, methods=["GET"])
+        self.add_url_rule("/tasks/<site_name>", view_func=OrganizationController.organization_view_all_tasks, methods=["GET"])
+        self.add_url_rule("/tasks/<task_id>/<site_name>", view_func=OrganizationController.organization_delete_task, methods=["DELETE"])
 
 organization_routes = OrganizationRoutes()
