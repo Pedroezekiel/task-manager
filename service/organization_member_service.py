989fd32feb66604e6fbc6bcbf7db49f7c826e553
pedroezekiel <ezekieleniola36>
2025-03-27 16:48:53 +0100

    added organization member

--- service/organization_member_service.py
+++ service/organization_member_service.py
@@ -1,5 +1,7 @@
 from flask import jsonify
 
+from enums.member_role_status import MemberRoleEnum
+from enums.task_status import TaskStatusEnum
 from models.organization_member import OrganizationMember
 from repositories.organization_member_repository import OrganizationMemberRepository
 from repositories.user_repository import UserRepository
@@ -15,6 +17,12 @@ class OrganizationMemberService:
         if user is None:
             return jsonify({"error":"User is not found"}), 401
         org_member = OrganizationMember(user_id=user["id"], org_id=organization["id"], site_name=organization["siteName"])
+        if data["role"] in data:
+            try:
+                MemberRoleEnum[data["role"]]
+            except KeyError:
+                return jsonify({"message": "Invalid status value"}), 400
+            org_member.role = data["role"]
         saved_org = OrganizationMemberRepository.save(org_member)
         return jsonify(saved_org), 200
 
@@ -26,3 +34,10 @@ class OrganizationMemberService:
         org_member = OrganizationMember(user_id=user_id, org_id=organization["id"], site_name=organization["siteName"])
         saved_org = OrganizationMemberRepository.save(org_member)
         return jsonify(saved_org), 200
+
+    @staticmethod
+    def view_organization_members(user_id, site_name):
+        if OrganizationMemberRepository.find_by_site_name_and_user_id(site_name, user_id) is None:
+            return jsonify({"error":"User is not in this organization"}), 401
+        organizations = OrganizationMemberRepository.find_all_by_site_name(site_name)
+        return jsonify({"organizations": organizations}), 200
