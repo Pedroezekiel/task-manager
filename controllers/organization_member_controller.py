c9964ad44ca638a4e0f287b6523a20cc7ada60f2
pedroezekiel <ezekieleniola36>
2025-03-27 16:05:10 +0100

    added organization member

---
+++ controllers/organization_member_controller.py
@@ -0,0 +1,19 @@
+from flask import request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+from service.organization_member_service import OrganizationMemberService
+
+class OrganizationMemberController:
+
+    @staticmethod
+    @jwt_required()
+    def add_organization_member():
+        data=request.get_json()
+        user_id = get_jwt_identity()
+        return OrganizationMemberService.add_member_to_organization(data, user_id)
+
+    @staticmethod
+    @jwt_required()
+    def member_join_organization(site_name):
+        user_id = get_jwt_identity()
+        return OrganizationMemberService.member_join_organization(user_id, site_name)
